b29c3672f8341ddb4240227dd1398524bfba3d70
Denis M. Gabaidulin <d.gabaydulin@zmeke.com>
2016-10-10 16:29:30 +0300

    * fix bugs

--- src/main/java/org/sherman/interview/misc/MaxSum.java
+++ src/main/java/org/sherman/interview/misc/MaxSum.java
@@ -20,26 +20,18 @@ public class MaxSum {
         List<Integer> max = new ArrayList<>();
 
         for (int i = 0; i < sequence.length; i++) {
-            if (sequence[i] >= 0) {
-                elts.add(sequence[i]);
-                currentSum += sequence[i];
+            if (currentSum + sequence[i] <= 0) {
+                currentSum = 0;
+                elts.clear();
             } else {
-                if (currentSum >= maxSum) {
+                currentSum += sequence[i];
+                elts.add(sequence[i]);
+                if (currentSum > maxSum) {
                     max.clear();
                     max.addAll(elts);
                     maxSum = currentSum;
                 }
-                currentSum = 0;
-                elts.clear();
             }
-        }
-
-        if (max.isEmpty()) {
-            return elts.toArray(new Integer[elts.size()]);
-        }
-
-        if (currentSum > maxSum) {
-            return elts.toArray(new Integer[elts.size()]);
         }
 
         return max.toArray(new Integer[max.size()]);
--- src/test/java/org/sherman/interview/misc/MaxSumTest.java
+++ src/test/java/org/sherman/interview/misc/MaxSumTest.java
@@ -12,10 +12,9 @@ public class MaxSumTest {
     @Test
     public void maxSum() {
         assertEquals(MaxSum.getMaxSum(new int[]{1, 2, 3}), new Integer[]{1, 2, 3});
-        assertEquals(MaxSum.getMaxSum(new int[]{0, -1}), new Integer[]{0});
-        assertEquals(MaxSum.getMaxSum(new int[]{1, 1, 1, -1, 2, 1, 2}), new Integer[]{2, 1, 2});
-        assertEquals(MaxSum.getMaxSum(new int[]{100, 0, -1, 99}), new Integer[]{100, 0});
-        assertEquals(MaxSum.getMaxSum(new int[]{-1, 0, 3, 4, 0, -1}), new Integer[]{0, 3, 4, 0});
+        assertEquals(MaxSum.getMaxSum(new int[]{1, 1, 1, -1, 2, 1, 2}), new Integer[]{1, 1, 1, -1, 2, 1, 2});
+        assertEquals(MaxSum.getMaxSum(new int[]{100, 0, -1, 99}), new Integer[]{100, 0, -1, 99});
+        assertEquals(MaxSum.getMaxSum(new int[]{-1, 0, 3, 4, 0, -1}), new Integer[]{3, 4});
         assertEquals(MaxSum.getMaxSum(new int[]{100, 200, -1000, 1, 2}), new Integer[]{100, 200});
     }
 }
