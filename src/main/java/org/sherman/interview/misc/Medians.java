56b23e7c9908ec13fe0b5789b852a0e81aae4762
Denis M. Gabaidulin <gabaden@gmail.com>
2016-10-30 21:45:59 +0300

    + get nth element

--- src/main/java/org/sherman/interview/misc/Medians.java
+++ src/main/java/org/sherman/interview/misc/Medians.java
@@ -47,4 +47,64 @@ public class Medians {
                 ? result[result.length / 2]
                 : (result[result.length / 2] + result[result.length / 2 - 1]) / 2.0;
     }
+
+    public static int getNthElement(@NotNull int[] data, int n) {
+        return _getNthElement(data, 0, data.length - 1, n);
+    }
+
+    private static int _getNthElement(int[] data, int left, int right, int n) {
+        log.info("l: {}, r: {}, data: {}", left, right, data);
+
+        // single element list case
+        if (left == right) {
+            return data[left];
+        }
+
+        int pivotIndex = partition(data, left, right);
+
+        //log.info("p: {}", pivotIndex);
+
+        if (pivotIndex == n) {
+            return data[pivotIndex];
+        } else if (n < pivotIndex) {
+            return _getNthElement(data, left, pivotIndex - 1, n);
+        } else {
+            return _getNthElement(data, pivotIndex + 1, right, n);
+        }
+    }
+
+    private static int partition(int[] data, int left, int right) {
+        int pivotIndex = (left + right) / 2;
+
+        int pivot = data[pivotIndex];
+
+        //log.info("pivot: {}", pivot);
+        //log.info("data: {}", data);
+
+        // move pivot to the right
+        swap(data, pivotIndex, right);
+
+        //log.info("data: {}", data);
+
+        int storeIndex = left;
+
+        for (int i = left; i < right; i++) {
+            if (data[i] < pivot) {
+                //log.info("{}, {}, data: {}", i, data[i], data);
+                swap(data, storeIndex, i);
+                storeIndex++;
+            }
+        }
+
+        // move pivot to it's final place
+        swap(data, right, storeIndex);
+
+        return storeIndex;
+    }
+
+    private static void swap(int[] data, int one, int two) {
+        int tmp = data[one];
+        data[one] = data[two];
+        data[two] = tmp;
+    }
 }
--- src/test/java/org/sherman/interview/misc/MediansTest.java
+++ src/test/java/org/sherman/interview/misc/MediansTest.java
@@ -16,4 +16,13 @@ public class MediansTest {
         assertEquals(Medians.getMedianOfSortedLists(new int[]{1, 2}, new int[]{2, 2, 2}), 2.0);
         assertEquals(Medians.getMedianOfSortedLists(new int[]{}, new int[]{1, 2, 3}), 2.0);
     }
+
+    @Test
+    public void getNthElement() {
+        assertEquals(Medians.getNthElement(new int[]{4, 6, 9, 11}, 0), 4);
+        assertEquals(Medians.getNthElement(new int[]{4, 6, 9, 11}, 1), 6);
+        assertEquals(Medians.getNthElement(new int[]{4, 6, 9, 11}, 2), 9);
+        assertEquals(Medians.getNthElement(new int[]{4, 6, 9, 11}, 3), 11);
+        assertEquals(Medians.getNthElement(new int[]{4, 67, 11, 11, 6, 9, 5}, 3), 9);
+    }
 }
