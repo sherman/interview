b90f7121438ecfb19b2a67b8c950a3a5928ed304
Denis M. Gabaidulin <gabaden@gmail.com>
2016-10-30 22:43:40 +0300

    + get a minimum subsequence of a given sum

--- src/main/java/org/sherman/interview/misc/SubsetSum.java
+++ src/main/java/org/sherman/interview/misc/SubsetSum.java
@@ -1,10 +1,13 @@
 package org.sherman.interview.misc;
 
+import com.google.common.primitives.Ints;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,20 +20,57 @@ public class SubsetSum {
     private SubsetSum() {
     }
 
-    public static Integer[] getIndexOfSum(@NotNull int[] array, int sum) {
+    public static int[] getIndexOfSum(@NotNull int[] data, int sum) {
         Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < array.length; i++) {
-            int value = sum - array[i];
+        for (int i = 0; i < data.length; i++) {
+            int value = sum - data[i];
 
             Integer saved = map.get(value);
             if (value >= 0 && saved != null) {
-                return new Integer[]{saved, i};
+                return new int[]{saved, i};
             } else {
-                map.put(array[i], i);
+                map.put(data[i], i);
             }
         }
 
-        return new Integer[]{};
+        return new int[]{};
+    }
+
+    public static int[] getShortestSubsetOfSum(@NotNull int[] data, int sum) {
+        int currentSum = 0;
+        List<Integer> indexes = new ArrayList<>();
+        List<Integer> found = new ArrayList<>();
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] <= sum) {
+                indexes.add(i);
+                currentSum += data[i];
+
+                if (currentSum >= sum) {
+                    log.info("{} {} {}", indexes, found, currentSum);
+
+                    // remove elements from the beginning
+                    int residual = currentSum - sum;
+                    while (residual > 0) {
+                        int removeElt = data[indexes.get(0)];
+                        residual = residual - removeElt;
+                        indexes.remove(0);
+                        currentSum = currentSum - removeElt;
+                    }
+
+                    if (residual == 0 && (found.isEmpty() || indexes.size() < found.size())) {
+                        // we've found a new solution!
+                        found.clear();
+                        found.addAll(indexes);
+                    }
+                }
+            } else {
+                indexes.clear();
+                currentSum = 0;
+            }
+        }
+
+        return Ints.toArray(found);
     }
 }
--- src/test/java/org/sherman/interview/misc/SubsetSumTest.java
+++ src/test/java/org/sherman/interview/misc/SubsetSumTest.java
@@ -1,10 +1,8 @@
 package org.sherman.interview.misc;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEqualsNoOrder;
-import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
 
 /**
  * @author Denis Gabaydulin
@@ -13,9 +11,18 @@ import static org.testng.AssertJUnit.assertEquals;
 public class SubsetSumTest {
     @Test
     public void getIndexOfSum() {
-        assertEqualsNoOrder(SubsetSum.getIndexOfSum(new int[]{5, 8, 7, 2}, 12), new Integer[]{0, 2});
-        assertEqualsNoOrder(SubsetSum.getIndexOfSum(new int[]{1, 1, 1, 1}, 12), new Integer[]{});
-        assertEqualsNoOrder(SubsetSum.getIndexOfSum(new int[]{1, 11, 1, 1}, 12), new Integer[]{0, 1});
-        assertEqualsNoOrder(SubsetSum.getIndexOfSum(new int[]{1, 2, 3, 4, 8}, 12), new Integer[]{3, 4});
+        assertArrayEquals(SubsetSum.getIndexOfSum(new int[]{5, 8, 7, 2}, 12), new int[]{0, 2});
+        assertArrayEquals(SubsetSum.getIndexOfSum(new int[]{1, 1, 1, 1}, 12), new int[]{});
+        assertArrayEquals(SubsetSum.getIndexOfSum(new int[]{1, 11, 1, 1}, 12), new int[]{0, 1});
+        assertArrayEquals(SubsetSum.getIndexOfSum(new int[]{1, 2, 3, 4, 8}, 12), new int[]{3, 4});
+    }
+
+    @Test
+    public void getShortestSubsetOfSum() {
+        assertArrayEquals(SubsetSum.getShortestSubsetOfSum(new int[]{2, 4, 3, 1, 7, 5}, 7), new int[]{4});
+        assertArrayEquals(SubsetSum.getShortestSubsetOfSum(new int[]{2, 4, 4, 1, 5, 5}, 7), new int[]{});
+        assertArrayEquals(SubsetSum.getShortestSubsetOfSum(new int[]{1, 1, 1, 2, 4, 4, 4, 4}, 3), new int[]{2, 3});
+        assertArrayEquals(SubsetSum.getShortestSubsetOfSum(new int[]{1, 1, 1, 1, 4, 4, 4, 4}, 3), new int[]{0, 1, 2});
+        assertArrayEquals(SubsetSum.getShortestSubsetOfSum(new int[]{1, 2, 3, 4, 5, 4, 5, 1}, 10), new int[]{5, 6, 7});
     }
 }
