6af25824584c5f8a5d7a6aed2ac9431330469e3c
Denis M. Gabaidulin <d.gabaydulin@zmeke.com>
2016-11-16 14:42:16 +0300

    * count routes in an acyclic graph (dynammic programming approach)

--- src/main/java/org/sherman/interview/misc/EnumerateRotes.java
+++ src/main/java/org/sherman/interview/misc/EnumerateRotes.java
@@ -2,6 +2,8 @@ package org.sherman.interview.misc;
 
 import com.google.common.collect.ImmutableList;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
@@ -10,18 +12,46 @@ import java.util.*;
  * @since 15/11/2016
  */
 public class EnumerateRotes {
+    private static final Logger log = LoggerFactory.getLogger(EnumerateRotes.class);
+
+    private static Map<Integer, Integer> verticesToCounts = new HashMap<>();
+
     private EnumerateRotes() {
     }
 
-    public static int enumerateRoutesBfs(@NotNull Map<Integer, List<Integer>> graph, int from, int to) {
+    public static int countRoutesBfs(@NotNull Map<Integer, List<Integer>> graph, int from, int to) {
         if (from == to) {
             return 1;
         } else {
             int count = 0;
 
             return Optional.ofNullable(graph.get(from)).orElse(ImmutableList.of()).stream()
-                    .map(v -> enumerateRoutesBfs(graph, v, to))
+                    .map(v -> countRoutesBfs(graph, v, to))
                     .reduce(count, (a, b) -> a + b);
+        }
+    }
+
+    public static int countRoutes(@NotNull boolean[][] graph, int from, int to) {
+        verticesToCounts.put(from, 1);
+        return countRoutes(graph, to);
+    }
+
+    private static int countRoutes(@NotNull boolean[][] graph, int v) {
+        log.info("{}", v);
+
+        Integer count = verticesToCounts.get(v);
+        if (count != null) {
+            return count;
+        } else {
+            count = 0;
+            for (int i = 1; i < graph.length; i++) {
+                // has edge i -> v
+                if (graph[i][v]) {
+                    count += countRoutes(graph, i);
+                }
+            }
+            verticesToCounts.put(v, count);
+            return count;
         }
     }
 }
--- src/test/java/org/sherman/interview/misc/EnumerateRotesTest.java
+++ src/test/java/org/sherman/interview/misc/EnumerateRotesTest.java
@@ -12,7 +12,19 @@ import static org.testng.Assert.assertEquals;
  */
 public class EnumerateRotesTest {
     @Test
-    public void enumerateRoutesBfs() {
-        assertEquals(EnumerateRotes.enumerateRoutesBfs(ImmutableMap.of(1, ImmutableList.of(2, 3), 2, ImmutableList.of(4, 5), 3, ImmutableList.of(5)), 1, 5), 2f);
+    public void countRoutesBfs() {
+        assertEquals(EnumerateRotes.countRoutesBfs(ImmutableMap.of(1, ImmutableList.of(2, 3), 2, ImmutableList.of(4, 5), 3, ImmutableList.of(5)), 1, 5), 2);
+    }
+
+    @Test
+    public void countRoutes() {
+        boolean[][] graph = new boolean[6][6];
+        graph[1][2] = true;
+        graph[1][3] = true;
+        graph[2][4] = true;
+        graph[2][5] = true;
+        graph[3][5] = true;
+
+        assertEquals(EnumerateRotes.countRoutes(graph, 1, 5), 2);
     }
 }
