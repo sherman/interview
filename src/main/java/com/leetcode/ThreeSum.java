03bf6d6d29422b2e808975e69c338a13438feb0b
Denis M. Gabaidulin <gabaden@gmail.com>
2018-03-04 11:55:49 +0300

    * found a solution with complexity of O(n^2)

--- src/main/java/com/leetcode/ThreeSum.java
+++ src/main/java/com/leetcode/ThreeSum.java
@@ -40,23 +40,39 @@ public class ThreeSum {
      * [-1, 0, 1],
      * [-1, -1, 2]
      * ]
+     * <p>
+     * O(n^2) solution
+     * </p>
      */
     public static List<List<Integer>> threeSum(int[] nums) {
         Set<Elements> elts = new HashSet<>();
 
+        Arrays.sort(nums);
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    log.info("{} {} {}", nums[i], nums[j], nums[k]);
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        Elements elements = new Elements();
-                        elements.i = nums[i];
-                        elements.j = nums[j];
-                        elements.k = nums[k];
-                        elts.add(elements);
-                    }
+            int l = 0;
+            int r = nums.length - 1;
+
+            int current = nums[i];
+            while (l < r && l != i && r != i) {
+                if (nums[l] + nums[r] + current < 0) {
+                    l++;
+                } else if (nums[l] + nums[r] + current > 0) {
+                    r--;
+                } else {
+                    log.info("f: {} {} {}", nums[i], nums[l], nums[r]);
+
+                    Elements elements = new Elements();
+                    elements.i = nums[i];
+                    elements.j = nums[l];
+                    elements.k = nums[r];
+                    elts.add(elements);
+
+                    l++;
+                    r--;
                 }
             }
+            log.info("{} {} {}", nums[i], l, r);
         }
 
         return elts.stream()
@@ -96,6 +112,7 @@ public class ThreeSum {
     public void test() {
         //threeSum(new int[]{-1, 0, 1, 2, -1, -4});
         //threeSum(new int[]{-1, 0, 1, 2, -1, -4});
-        threeSum(new int[]{-2, -7, -11, -8, 9, -7, -8, -15, 10, 4, 3, 9, 8, 11, 1, 12, -6, -14, -2, -1, -7, -13, -11, -15, 11, -2, 7, -4, 12, 7, -3, -5, 7, -7, 3, 2, 1, 10, 2, -12, -1, 12, 12, -8, 9, -9, 11, 10, 14, -6, -6, -8, -3, -2, 14, -15, 3, -2, -4, 1, -9, 8, 11, 5, -14, -1, 14, -6, -14, 2, -2, -8, -9, -13, 0, 7, -7, -4, 2, -8, -2, 11, -9, 2, -13, -10, 2, 5, 4, 13, 13, 2, -1, 10, 14, -8, -14, 14, 2, 10});
+        threeSum(new int[]{3, 0, -2, -1, 1, 2}); // -2 -1 0 1 2 3
+        //threeSum(new int[]{-2, -7, -11, -8, 9, -7, -8, -15, 10, 4, 3, 9, 8, 11, 1, 12, -6, -14, -2, -1, -7, -13, -11, -15, 11, -2, 7, -4, 12, 7, -3, -5, 7, -7, 3, 2, 1, 10, 2, -12, -1, 12, 12, -8, 9, -9, 11, 10, 14, -6, -6, -8, -3, -2, 14, -15, 3, -2, -4, 1, -9, 8, 11, 5, -14, -1, 14, -6, -14, 2, -2, -8, -9, -13, 0, 7, -7, -4, 2, -8, -2, 11, -9, 2, -13, -10, 2, 5, 4, 13, 13, 2, -1, 10, 14, -8, -14, 14, 2, 10});
     }
 }
