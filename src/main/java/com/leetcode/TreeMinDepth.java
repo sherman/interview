e745b3b756c81858dcc39e297aab7364947abf48
Denis M. Gabaidulin <gabaden@gmail.com>
2018-03-04 19:14:53 +0300

    * tree min depth

---
+++ src/main/java/com/leetcode/TreeMinDepth.java
@@ -0,0 +1,41 @@
+package com.leetcode;
+
+/*
+ * Copyright (C) 2018 by Denis M. Gabaydulin
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class TreeMinDepth {
+    /**
+     * Given a binary tree, find its minimum depth.
+     * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+     */
+    public static int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+
+        if (left == 0 || right == 0) {
+            return left + right + 1; // node it terminal by left or right side
+        } else {
+            return Math.min(left, right) + 1;
+        }
+    }
+}
