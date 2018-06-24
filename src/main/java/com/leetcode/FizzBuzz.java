32c069720e5d14f55007a556bd225cfd87d908ad
Denis M. Gabaidulin <gabaden@gmail.com>
2018-06-24 19:56:54 +0300

    412. Fizz Buzz

---
+++ src/main/java/com/leetcode/FizzBuzz.java
@@ -0,0 +1,43 @@
+package com.leetcode;
+
+/*
+ * Copyright (C) 2018 by Denis M. Gabaydulin
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>(n);
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+
+        return result;
+    }
+}
