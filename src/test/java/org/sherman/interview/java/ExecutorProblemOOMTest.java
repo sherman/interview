15b1cc7b916c44714d3c1d7b0e33fef9eb9124fd
Gabaydulin Denis <denis.gabaydulin@corp.mail.ru>
2019-02-17 11:11:14 +0300

    * Using discard oldest policy

--- src/test/java/org/sherman/interview/java/ExecutorProblemOOMTest.java
+++ src/test/java/org/sherman/interview/java/ExecutorProblemOOMTest.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.*;
 
 /**
@@ -94,6 +95,70 @@ public class ExecutorProblemOOMTest {
                 log.error("Too many tasks in the work");
                 break;
             }
+        }
+
+        executorService.shutdown();
+    }
+
+    /**
+     * Expected:
+     * <p>
+     * java.lang.StackOverflowError, because SynchronousQueue based on stack impl. of the transfer.
+     */
+    @Test(expectedExceptions = java.lang.StackOverflowError.class)
+    public void discardOldestPolicyStackOverFlow() {
+        ExecutorService executorService = new ThreadPoolExecutor(
+            10,
+            10,
+            0L, TimeUnit.MILLISECONDS,
+            new SynchronousQueue<>(),
+            new ThreadFactoryBuilder()
+                .setDaemon(true)
+                .setNameFormat("oom-worker-%s")
+                .build(),
+            new ThreadPoolExecutor.DiscardOldestPolicy()
+        );
+
+        while (true) {
+            try {
+                executorService.submit(() -> {
+                    try {
+                        Thread.sleep(Integer.MAX_VALUE);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (RejectedExecutionException e) {
+                log.error("Too many tasks in the work");
+                break;
+            }
+        }
+
+        executorService.shutdown();
+    }
+
+    @Test
+    public void discardOldestPolicy() {
+        ExecutorService executorService = new ThreadPoolExecutor(
+            10,
+            10,
+            0L, TimeUnit.MILLISECONDS,
+            new ArrayBlockingQueue<Runnable>(100),
+            new ThreadFactoryBuilder()
+                .setDaemon(true)
+                .setNameFormat("oom-worker-%s")
+                .build(),
+            new ThreadPoolExecutor.DiscardOldestPolicy()
+        );
+
+        for (int i = 0; i < 1000; i++) {
+            executorService.submit(() -> {
+                try {
+                    Thread.sleep(Integer.MAX_VALUE);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
         }
 
         executorService.shutdown();
