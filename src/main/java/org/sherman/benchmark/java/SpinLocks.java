59ee82f0b363d652a90b3c5bb78b92f7152f65fa
Denis M. Gabaidulin <gabaden@gmail.com>
2019-11-18 20:02:36 +0300

    * fix spin lock and show the difference

---
+++ src/main/java/org/sherman/benchmark/java/SpinLocks.java
@@ -0,0 +1,51 @@
+package org.sherman.benchmark.java;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+import org.sherman.benchmark.one.hash.Maps;
+import org.sherman.interview.java.SimpleLock;
+import org.sherman.interview.java.TestAndSetLock;
+import org.sherman.interview.java.TestAndTestAndSetLock;
+
+@Fork(2)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class SpinLocks {
+
+    private SimpleLock testAndSetLock = new TestAndSetLock();
+    private SimpleLock testAndTestAndSetLock = new TestAndTestAndSetLock();
+
+    @Benchmark
+    public void testAndSetLock(Blackhole blackhole) {
+        testAndSetLock.lock();
+        try {
+            blackhole.consume(42L);
+        } finally {
+            testAndSetLock.unlock();
+        }
+
+    }
+
+    @Benchmark
+    public void testAndTestAndSetLock(Blackhole blackhole) {
+        testAndTestAndSetLock.lock();
+        try {
+            blackhole.consume(42L);
+        } finally {
+            testAndTestAndSetLock.unlock();
+        }
+    }
+}
--- src/main/java/org/sherman/interview/java/TestAndSetLock.java
+++ src/main/java/org/sherman/interview/java/TestAndSetLock.java
@@ -3,7 +3,7 @@ package org.sherman.interview.java;
 public class TestAndSetLock extends BaseLock {
     @Override
     public void lock() {
-        while (locked.compareAndSet(false, true)) {
+        while (locked.getAndSet( true)) {
             // do nothing
         }
     }
