75ee182f37e6d220b65fda85b68f287653b4bb0c
Denis M. Gabaidulin <gabaden@gmail.com>
2020-09-16 14:32:08 +0300

    + tricky answer for 238

--- src/main/java/com/leetcode/fb/ProductOfArrayExceptSelf.java
+++ src/main/java/com/leetcode/fb/ProductOfArrayExceptSelf.java
@@ -5,20 +5,30 @@ import org.testng.internal.junit.ArrayAsserts;
 
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
-        int total = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            total = total * nums[i];
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        left[0] = 1;
+        right[right.length - 1] = 1;
+
+        for (int i = 1; i < left.length; i++) {
+            left[i] = nums[i - 1] * left[i - 1];
+        }
+
+        for (int i = right.length - 2; i >= 0; i--) {
+            right[i] = nums[i + 1] * right[i + 1];
         }
 
         int[] result = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            result[i] = total / nums[i];
+            result[i] = left[i] * right[i];
         }
+
         return result;
     }
 
     @Test
     public void test() {
         ArrayAsserts.assertArrayEquals(productExceptSelf(new int[]{1, 2, 3, 4}), new int[]{24, 12, 8, 6});
+        ArrayAsserts.assertArrayEquals(productExceptSelf(new int[]{0, 1}), new int[]{1, 0});
     }
 }
