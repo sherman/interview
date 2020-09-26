295742f8453da2378e7dfaeb77364dfe0d33c489
Denis M. Gabaidulin <gabaden@gmail.com>
2020-09-26 12:51:27 +0300

    721. Accounts Merg (via dfs: getting connected components)

--- src/main/java/com/leetcode/fb/AccountsMerge.java
+++ src/main/java/com/leetcode/fb/AccountsMerge.java
@@ -2,14 +2,13 @@ package com.leetcode.fb;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 import java.util.TreeSet;
 
 import com.google.common.collect.ImmutableList;
-import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -19,60 +18,61 @@ public class AccountsMerge {
     private static final Logger logger = LoggerFactory.getLogger(AccountsMerge.class);
 
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        Map<Integer, String> names = new HashMap<>();
-        // Build an index which: email points to an index in the original array.
-        Map<String, Set<Integer>> index = new HashMap<>();
+        // 1). Create mapping email to name
+        Map<String, String> emailsToNames = new HashMap<>();
         for (int i = 0; i < accounts.size(); i++) {
-            names.put(i, accounts.get(i).get(0));
+            String name = accounts.get(i).get(0);
             for (int j = 1; j < accounts.get(i).size(); j++) {
-                Set<Integer> accountIndex = index.getOrDefault(accounts.get(i).get(j), new HashSet<>());
-                accountIndex.add(i);
-                index.put(accounts.get(i).get(j), accountIndex);
+                emailsToNames.put(accounts.get(i).get(j), name);
             }
         }
 
-        //logger.info("{}", index);
-
-        // Then remap indexes, starts from 0. Replace all indexes in a single row with the same value.
-        // Replace all equals values by a single value.
-        Map<Integer, Pair<Integer, String>> remap = new HashMap<>();
-        int id = 0;
-        for (String email : index.keySet()) {
-            for (int i : index.get(email)) {
-                if (remap.get(i) != null) {
-                    remap.put(i, remap.get(i));
+        // 2). Build undirected graph
+        Map<String, List<String>> graph = new HashMap<>();
+        for (int i = 0; i < accounts.size(); i++) {
+            // skip name
+            for (int j = 1; j < accounts.get(i).size(); j++) {
+                String firstEmail = accounts.get(i).get(1);
+                if (j == 1) {
+                    graph.computeIfAbsent(firstEmail, ignored -> new ArrayList<>());
                 } else {
-                    remap.put(i, Pair.of(id, names.get(i)));
+                    String email = accounts.get(i).get(j);
+                    graph.computeIfAbsent(firstEmail, ignored -> new ArrayList<>()).add(email);
+                    graph.computeIfAbsent(email, ignored -> new ArrayList<>()).add(firstEmail);
                 }
             }
-            id++;
         }
 
-        //logger.info("{}", remap);
+        // 3). Find connected components
+        Map<String, Set<String>> components = new HashMap<>();
+        Set<String> globalVisited = new TreeSet<>();
+        for (String email : graph.keySet()) {
+            if (!globalVisited.contains(email)) {
+                Set<String> component = new TreeSet<>();
 
-        // Remap email index to new keys
-        Map<String, Pair<Integer, String>> remapped = new HashMap<>();
-        for (String email : index.keySet()) {
-            for (int i : index.get(email)) {
-                Pair<Integer, String> key = remap.get(i);
-                remapped.put(email, key);
-                break;
+                Stack<String> stack = new Stack<>();
+                stack.push(email);
+                while (!stack.isEmpty()) {
+                    String v = stack.pop();
+                    component.add(v);
+                    globalVisited.add(v);
+                    for (String connected : graph.get(v)) {
+                        if (!globalVisited.contains(connected)) {
+                            stack.push(connected);
+                        }
+                    }
+                }
+
+                components.put(email, component);
             }
         }
 
-        // Group by key
-        Map<Pair<Integer, String>, TreeSet<String>> mergedAccounts = new HashMap<>();
-        for (String email : remapped.keySet()) {
-            TreeSet<String> emails = mergedAccounts.getOrDefault(remapped.get(email), new TreeSet<>());
-            emails.add(email);
-            mergedAccounts.put(remapped.get(email), emails);
-        }
-
+        // 4). Prepare result
         List<List<String>> result = new ArrayList<>();
-        for (Pair<Integer, String> pair : mergedAccounts.keySet()) {
+        for (String email : components.keySet()) {
             List<String> data = new ArrayList<>();
-            data.add(pair.getValue());
-            data.addAll(mergedAccounts.get(pair));
+            data.add(emailsToNames.get(email));
+            data.addAll(components.get(email));
             result.add(data);
         }
 
