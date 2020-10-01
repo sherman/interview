bc3242ce1871604db506a607373b411d485ef7dd
Denis M. Gabaidulin <gabaden@gmail.com>
2020-10-01 11:02:24 +0300

    * simple read file

---
+++ src/main/java/org/sherman/interview/java/ReadFiles.java
@@ -0,0 +1,33 @@
+package org.sherman.interview.java;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReadFiles {
+    private static final Logger logger = LoggerFactory.getLogger(ReadFiles.class);
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("/home/dgabaydulin//file");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        char[] buffer = new char[8192];
+        int read = 0;
+        int offset = 0;
+        StringBuilder b = new StringBuilder();
+        while ((read = reader.read(buffer)) != -1) {
+            b.append(buffer, offset, read);
+            offset += read;
+
+        }
+        String result = b.toString();
+        logger.info("{}", result);
+    }
+}
