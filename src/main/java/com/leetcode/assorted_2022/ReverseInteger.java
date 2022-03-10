a0a6373eaf764c1749a42171a6b22d35f7e52f8b
Denis M. Gabaidulin <gabaden@gmail.com>
2022-03-10 21:35:20 +0300

    7. Reverse Integer

---
+++ src/main/java/com/leetcode/assorted_2022/ReverseInteger.java
@@ -0,0 +1,33 @@
+package com.leetcode.assorted_2022;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ReverseInteger {
+    private static final Logger logger = LoggerFactory.getLogger(ReverseInteger.class);
+
+    public static int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x = x / 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(reverse(120), 21);
+        Assert.assertEquals(reverse(31), 13);
+        Assert.assertEquals(reverse(1534236469), 0);
+    }
+}
