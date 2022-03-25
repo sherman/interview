223c6b1f016ac89ba7722b8828620dbb900be41f
Denis M. Gabaidulin <gabaden@gmail.com>
2022-03-25 23:48:15 +0300

    Basic BST questions

---
+++ src/test/java/com/leetcode/FindNextValueInBST.java
@@ -0,0 +1,98 @@
+package com.leetcode;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FindNextValueInBST {
+    private static final Logger logger = LoggerFactory.getLogger(FindNextValueInBST.class);
+
+    public Integer getNextValue(TreeNode root, int value) {
+        if (root == null) {
+            return null;
+        }
+
+        var left = getNextValue(root.left, value);
+        if (left != null) {
+            return left;
+        }
+
+        if (root.val > value) {
+            logger.info("Value: [{}]", root.val);
+            return root.val;
+        }
+
+        return getNextValue(root.right, value);
+    }
+
+    public void inOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrderTraversal(root.left);
+        logger.info("Value: [{}]", root.val);
+        inOrderTraversal(root.right);
+
+    }
+
+    public void preOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        logger.info("Value: [{}]", root.val);
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+
+    }
+
+    public void postOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        logger.info("Value: [{}]", root.val);
+    }
+
+    /**
+     * 50
+     * /      \
+     * 30       80
+     * /   \     /   \
+     * 20   40   60  100
+     * /  \   /  \
+     * 10   25  31  42
+     */
+    @Test
+    public void test() {
+        var root = TreeBuilder.build(new Integer[] {50, 30, 80, 20, 40, 60, 100, 10, 25, 31, 42});
+        Assert.assertEquals(getNextValue(root, 21), Integer.valueOf(25));
+        Assert.assertEquals(getNextValue(root, 50), Integer.valueOf(60));
+        Assert.assertEquals(getNextValue(root, 30), Integer.valueOf(31));
+        Assert.assertEquals(getNextValue(root, 60), Integer.valueOf(80));
+        Assert.assertEquals(getNextValue(root, 25), Integer.valueOf(30));
+    }
+
+    private static class TreeBuilder {
+        public static TreeNode build(Integer[] data) {
+            TreeNode root = new TreeNode(data[0]);
+            build(root, data, 0);
+            return root;
+        }
+
+        private static void build(TreeNode node, Integer[] data, int index) {
+            if (index >= data.length / 2) {
+                return;
+            }
+
+            node.left = new TreeNode(data[2 * index + 1]);
+            node.right = new TreeNode(data[2 * index + 2]);
+            build(node.left, data, 2 * index + 1);
+            build(node.right, data, 2 * index + 2);
+        }
+    }
+}
