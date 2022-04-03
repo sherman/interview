1ed616c0bedcdac20b091f45eed19b1a1703472e
Denis M. Gabaidulin <gabaden@gmail.com>
2022-04-03 19:53:16 +0300

    * cosmetics

--- src/main/java/com/leetcode/assorted_2022/LongestIncreasingSubsequence.java
+++ src/main/java/com/leetcode/assorted_2022/LongestIncreasingSubsequence.java
@@ -34,7 +34,7 @@ public class LongestIncreasingSubsequence {
 
     @Test
     public void test() {
-        //Assert.assertEquals(lengthOfLIS(new int[] {10, 9, 2, 5, 3, 7, 101, 18}), 4);
+        Assert.assertEquals(lengthOfLIS(new int[] {10, 9, 2, 5, 3, 7, 101, 18}), 4);
         Assert.assertEquals(lengthOfLIS(new int[] {0, 1, 0, 3, 2, 3}), 4);
     }
 }
