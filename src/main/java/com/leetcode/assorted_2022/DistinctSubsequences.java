fa74bc66bbdceb014d8d6dda08854c9bad574b91
Denis M. Gabaidulin <gabaden@gmail.com>
2022-04-12 19:14:39 +0300

    115. Distinct Subsequences (memoization)

--- src/main/java/com/leetcode/assorted_2022/DistinctSubsequences.java
+++ src/main/java/com/leetcode/assorted_2022/DistinctSubsequences.java
@@ -15,34 +15,50 @@ public class DistinctSubsequences {
     private Map<Call, Integer> cache = new HashMap<>();
 
     public int numDistinct(String s, String t) {
+        cache.clear();
+
         var chars = s.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
         var expected = t.chars().mapToObj(c -> (char) c).toList();
-        return call(chars, expected, 0);
+        return call(chars, expected, 0, 0);
     }
 
-    private int call(List<Character> s, List<Character> expected, int index) {
-        if (s.equals(expected)) {
-            return 1;
+    private int call(List<Character> s, List<Character> t, int sIndex, int tIndex) {
+        logger.info("[{}] [{}]", sIndex, tIndex);
+
+        // base case
+        if (sIndex == s.size()
+            || tIndex == t.size()
+            || s.size() - sIndex < t.size() - tIndex
+        ) {
+            return tIndex == t.size() ? 1 : 0;
         }
 
-        var sum = 0;
-        for (var i = index; i < s.size(); i++) {
-            var c = s.remove(i);
-            var result = call(s, expected, i);
-            s.add(i, c);
-            sum += result;
+
+        var key = new Call(sIndex, tIndex);
+        if (cache.containsKey(key)) {
+            return cache.get(key);
         }
 
-        return sum;
+        // search current char of T in next index of S
+        int count = call(s, t, sIndex + 1, tIndex);
+
+        // current char matched, look for next char
+        if (s.get(sIndex) == t.get(tIndex)) {
+            count += call(s, t, sIndex + 1, tIndex + 1);
+        }
+
+        cache.put(key, count);
+
+        return count;
     }
 
     @Test
     public void test() {
         Assert.assertEquals(numDistinct("babgbag", "bag"), 5);
         Assert.assertEquals(numDistinct("rabbbit", "rabbit"), 3);
-        //Assert.assertEquals(numDistinct("bccbcdcabadabddbccaddcbabbaaacdba", "bccbbdc"), 3);
+        Assert.assertEquals(numDistinct("bccbcdcabadabddbccaddcbabbaaacdba", "bccbbdc"), 172);
     }
 
-    private record Call(List<Character> prefix, int index) {
+    private record Call(int index1, int index2) {
     }
 }
