6df9a2a334b71ab192180c5d628e26cd8b6ab814
Denis M. Gabaidulin <gabaden@gmail.com>
2022-08-10 21:15:42 +0300

    322. Coin Change (memoization)

--- src/main/java/com/leetcode/assorted_2022/CoinChange.java
+++ src/main/java/com/leetcode/assorted_2022/CoinChange.java
@@ -1,5 +1,7 @@
 package com.leetcode.assorted_2022;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -7,39 +9,50 @@ import org.testng.annotations.Test;
 
 public class CoinChange {
     private static final Logger logger = LoggerFactory.getLogger(CoinChange.class);
+    private Map<Integer, Integer> cache = new HashMap<>();
 
     public int coinChange(int[] coins, int amount) {
-        var res = dp(coins, amount, 0);
-        if (res == Integer.MAX_VALUE) {
+        cache.clear();
+
+        var res = dp(coins, amount);
+        if (res == 10000) {
             return -1;
         } else {
             return res;
         }
     }
 
-    public int dp(int[] coins, int amount, int coinsNumber) {
-        //logger.info("[{}]", amount);
-        if (amount == 0) {
-            return coinsNumber;
+    public int dp(int[] coins, int amount) {
+        var result = cache.get(amount);
+        if (result != null) {
+            return result;
         }
 
-        var current = Integer.MAX_VALUE;
+        //logger.info("[{}]", amount);
+        if (amount == 0) {
+            return 0;
+        }
+
+        var current = 10000;
         for (var coin : coins) {
             if (amount - coin >= 0) {
-                var ans = dp(coins, amount - coin, coinsNumber + 1);
+                var ans = dp(coins, amount - coin) + 1;
                 if (current > ans) {
                     current = ans;
                 }
             }
         }
 
+        cache.put(amount, current);
         return current;
     }
 
     @Test
     public void test() {
-        Assert.assertEquals(3, coinChange(new int[] {1, 2, 5}, 11));
-        Assert.assertEquals(-1, coinChange(new int[] {2}, 3));
-        Assert.assertEquals(0, coinChange(new int[] {1}, 0));
+        Assert.assertEquals(coinChange(new int[] {1, 2, 5}, 11), 3);
+        Assert.assertEquals(coinChange(new int[] {2}, 3), -1);
+        Assert.assertEquals(coinChange(new int[] {1}, 0), 0);
+        Assert.assertEquals(coinChange(new int[] {3, 7, 405, 436}, 8839), 25);
+        //Assert.assertEquals(coinChange(new int[] {1}, 10000), 10000);
     }
 }
