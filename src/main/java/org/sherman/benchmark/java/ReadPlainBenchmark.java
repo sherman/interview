b1b53151cd2770c3bcb2e6542bcb48b50316377e
Denis M. Gabaidulin <gabaden@gmail.com>
2022-11-22 21:21:52 +0300

    * Read plain

---
+++ src/main/java/org/sherman/benchmark/java/ReadPlainBenchmark.java
@@ -0,0 +1,35 @@
+package org.sherman.benchmark.java;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+@Fork(2)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class ReadPlainBenchmark {
+    private final AtomicBoolean atomicInvalidated = new AtomicBoolean(false);
+    private boolean invalidated;
+
+    @Benchmark
+    public void plain(Blackhole blackhole) {
+        blackhole.consume(invalidated);
+    }
+
+    @Benchmark
+    public void atomic(Blackhole blackhole) {
+        blackhole.consume(atomicInvalidated.getPlain());
+    }
+}
