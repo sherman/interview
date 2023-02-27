9420aba71634cd1980b0e82ba54cb42c65d3e228
Denis M. Gabaidulin <gabaden@gmail.com>
2023-02-27 21:13:44 +0300

    * try nalim

---
+++ src/main/java/org/sherman/benchmark/one/nalim/Cpu.java
@@ -0,0 +1,17 @@
+package org.sherman.benchmark.one.nalim;
+
+import one.nalim.Code;
+import one.nalim.Linker;
+
+public class Cpu {
+    // rdtsc
+    // shl    $0x20,%rdx
+    // or     %rdx,%rax
+    // ret
+    @Code("0f31 48c1e220 4809d0 c3")
+    public static native long rdtsc();
+
+    static {
+        Linker.linkClass(Cpu.class);
+    }
+}
---
+++ src/main/java/org/sherman/benchmark/one/nalim/TimerBenchmark.java
@@ -0,0 +1,31 @@
+package org.sherman.benchmark.one.nalim;
+
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+@Fork(2)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class TimerBenchmark {
+    @Benchmark
+    public void nanoTime(Blackhole blackhole) {
+        blackhole.consume(System.nanoTime());
+    }
+
+    @Benchmark
+    public void rdtsc(Blackhole blackhole) {
+        blackhole.consume(Cpu.rdtsc());
+    }
+}
