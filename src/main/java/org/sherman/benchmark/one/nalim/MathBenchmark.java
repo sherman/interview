39f97c79cdad121c1b0c7d00e33c468194568304
Denis M. Gabaidulin <gabaden@gmail.com>
2023-02-28 22:03:11 +0300

    * trpt benchmark is more interesting here

--- src/main/java/org/sherman/benchmark/one/nalim/MathBenchmark.java
+++ src/main/java/org/sherman/benchmark/one/nalim/MathBenchmark.java
@@ -1,5 +1,6 @@
 package org.sherman.benchmark.one.nalim;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -8,6 +9,7 @@ import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
@@ -15,18 +17,33 @@ import org.openjdk.jmh.infra.Blackhole;
 @Fork(2)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
 public class MathBenchmark {
+    private final Random r = new Random();
+    private static final int SIZE = 1024;
+    private final int[] data = new int[SIZE];
+
+    @Setup
+    public void generate() {
+        for (int i = 0; i < SIZE; i++) {
+            data[i] = i;
+        }
+    }
+
     @Benchmark
     public void javaSquare(Blackhole blackhole) {
-        blackhole.consume(square(32));
+        for (var i = 0; i < data.length; i++) {
+            blackhole.consume(square(data[i]));
+        }
     }
 
     @Benchmark
     public void nalimSquare(Blackhole blackhole) {
-        blackhole.consume(Math.square(32));
+        for (var i = 0; i < data.length; i++) {
+            blackhole.consume(Math.square(data[i]));
+        }
     }
 
     private static int square(int value) {
