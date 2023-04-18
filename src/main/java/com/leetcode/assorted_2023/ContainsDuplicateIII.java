227e3f695a49c1ba1d329c2418af7a054573d0f6
Denis M. Gabaidulin <gabaden@gmail.com>
2023-04-18 22:26:38 +0300

    220. Contains Duplicate III (complexity: O(n log k), space: O(k) )

--- src/main/java/com/leetcode/assorted_2023/ContainsDuplicateIII.java
+++ src/main/java/com/leetcode/assorted_2023/ContainsDuplicateIII.java
@@ -2,6 +2,7 @@ package com.leetcode.assorted_2023;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.TreeSet;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -24,9 +25,41 @@ public class ContainsDuplicateIII {
         return false;
     }
 
+    public boolean containsNearbyDuplicate2(int[] nums, int k, int m) {
+        var set = new TreeSet<Long>();
+
+        for (int i = 0; i < nums.length; i++) {
+            Long num = (long) nums[i];
+            var floor = set.floor(num);
+            var ceil = set.ceiling(num);
+
+            if (floor != null && Math.abs(floor - num) <= m) {
+                return true;
+            }
+
+            if (ceil != null && Math.abs(ceil - num) <= m) {
+                return true;
+            }
+
+            set.add(num);
+
+            if (set.size() > k) {
+                set.remove((long) nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+
     @Test
-    public void test() {
+    public void test1() {
         Assert.assertTrue(containsNearbyDuplicate(new int[] {1, 2, 3, 1}, 3, 0));
         Assert.assertFalse(containsNearbyDuplicate(new int[] {1, 5, 9, 1, 5, 9}, 2, 3));
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertTrue(containsNearbyDuplicate2(new int[] {1, 2, 3, 1}, 3, 0));
+        Assert.assertFalse(containsNearbyDuplicate2(new int[] {1, 5, 9, 1, 5, 9}, 2, 3));
     }
 }
