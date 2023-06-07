d418c8f7e5a48e7cee60bde4e5cffd236386cfcb
Denis M. Gabaidulin <gabaden@gmail.com>
2023-06-07 23:11:10 +0300

    739. Daily Temperatures (monotonic stack)

--- src/main/java/com/leetcode/assorted_2023/DailyTemperature.java
+++ src/main/java/com/leetcode/assorted_2023/DailyTemperature.java
@@ -10,37 +10,21 @@ public class DailyTemperature {
     private static final Logger logger = LoggerFactory.getLogger(DailyTemperature.class);
 
     public int[] dailyTemperatures(int[] temperatures) {
-        var current = temperatures[0];
-
         var result = new int[temperatures.length];
 
         var stack = new Stack<Integer>();
-        for (var i = 1; i < temperatures.length; i++) {
-            var next = temperatures[i];
-            if (next <= current) {
-                stack.push(next);
-            } else {
-                var c = 0;
-                while (!stack.isEmpty()) {
-                    stack.pop();
-                    c++;
-                }
-                i = i - c;
-                result[i - 1] = c + 1;
-                current = temperatures[i];
+        for (var i = temperatures.length - 1; i >= 0; i--) {
+            logger.info("[{}]", stack);
+            var current = temperatures[i];
+            while (!stack.isEmpty() && current >= temperatures[stack.peek()]) {
+                stack.pop();
             }
 
-            if (i == temperatures.length - 1 && !stack.isEmpty()) {
-                var c = 0;
-                while (!stack.isEmpty()) {
-                    stack.pop();
-                    c++;
-                }
-                i = i - c;
-                result[i] = 0;
-                i++;
-                current = temperatures[i];
+            if (!stack.isEmpty()) {
+                result[i] = stack.peek() - i;
             }
+
+            stack.push(i);
         }
 
         logger.info("R: [{}]", result);
@@ -51,7 +35,7 @@ public class DailyTemperature {
     @Test
     public void test() {
         Assert.assertEquals(dailyTemperatures(
-            new int[] {55, 38, 53, 81, 61, 93, 97, 32, 43, 78}),
+                new int[] {55, 38, 53, 81, 61, 93, 97, 32, 43, 78}),
             new int[] {3, 1, 1, 2, 1, 1, 0, 1, 1, 0}
         );
         Assert.assertEquals(dailyTemperatures(new int[] {73, 74, 75, 71, 69, 72, 76, 73}), new int[] {1, 1, 4, 2, 1, 1, 0, 0});
