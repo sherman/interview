8be181523bccc7631736d9dff840338b988782b5
Denis M. Gabaidulin <gabaden@gmail.com>
2023-09-20 23:41:30 +0300

    * very naive test for reading a memory segment (using VH)

--- src/main/java/org/sherman/benchmark/java/OffHeapReadPrimitiveBenchmark.java
+++ src/main/java/org/sherman/benchmark/java/OffHeapReadPrimitiveBenchmark.java
@@ -1,42 +1,27 @@
 package org.sherman.benchmark.java;
 
-import static com.google.common.primitives.Ints.fromBytes;
-import static java.lang.foreign.ValueLayout.JAVA_BYTE;
-import static java.lang.foreign.ValueLayout.JAVA_INT;
-
 import com.google.common.io.Files;
-import java.io.File;
-import java.io.IOException;
-import java.lang.foreign.Arena;
-import java.lang.foreign.MemorySegment;
-import java.lang.foreign.ValueLayout;
-import java.nio.channels.FileChannel;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
-
 import com.google.common.primitives.Ints;
 import one.nio.mem.MappedFile;
 import one.nio.util.JavaInternals;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
-import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.misc.Unsafe;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.VarHandle;
+import java.nio.channels.FileChannel;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+import static com.google.common.primitives.Ints.fromBytes;
+import static java.lang.foreign.ValueLayout.JAVA_BYTE;
 
 @Fork(2)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
@@ -48,7 +33,11 @@ public class OffHeapReadPrimitiveBenchmark {
     private static final Logger logger = LoggerFactory.getLogger(OffHeapReadPrimitiveBenchmark.class);
 
     private static final ValueLayout.OfByte LAYOUT = JAVA_BYTE;
+
+    private static final VarHandle VAR_HANDLE = JAVA_BYTE.arrayElementVarHandle();
+
     private static final int SIZE = 1 << 20;
+    private static final SequenceLayout SEQUENCE_LAYOUT = MemoryLayout.sequenceLayout(SIZE / 4, ValueLayout.JAVA_INT);
     private final byte[] data = new byte[SIZE];
     private static final Unsafe unsafe = JavaInternals.getUnsafe();
     private MemorySegment memorySegment;
@@ -93,10 +82,11 @@ public class OffHeapReadPrimitiveBenchmark {
     @Benchmark
     public void readSegment(Blackhole blackhole) {
         var index = ThreadLocalRandom.current().nextLong(intsAsLong) * 4;
-        var b1 = memorySegment.get(LAYOUT, index);
-        var b2 = memorySegment.get(LAYOUT, index + 1);
-        var b3 = memorySegment.get(LAYOUT, index + 2);
-        var b4 = memorySegment.get(LAYOUT, index + 3);
+        var b1 = (byte) VAR_HANDLE.get(memorySegment, index);
+        var b2 = (byte) VAR_HANDLE.get(memorySegment, index + 1);
+        var b3 = (byte) VAR_HANDLE.get(memorySegment, index + 2);
+        var b4 = (byte) VAR_HANDLE.get(memorySegment, index + 3);
+        //logger.info("[{}]", fromBytes(b1, b2, b3, b4));
         blackhole.consume(fromBytes(b1, b2, b3, b4));
     }
 }
