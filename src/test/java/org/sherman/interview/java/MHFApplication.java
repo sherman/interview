22178a0cc20a0875f44089a6c636027fa8a93679
Denis M. Gabaidulin <gabaden@gmail.com>
2023-10-16 18:20:39 +0300

    * see how signed signitures work

--- src/test/java/org/sherman/interview/java/MHFApplication.java
+++ src/test/java/org/sherman/interview/java/MHFApplication.java
@@ -17,10 +17,14 @@ public class MHFApplication {
             .keys(IntStream.range(1, 1025).boxed().map(Ints::toByteArray).toList())
             .numKeys(1024)
             .transform(TransformationStrategies.byteArray())
+            .signed(1024)
             .build();
 
         for (var i = 1; i <= 1024; i++) {
             logger.info("[{}]", mhf.getLong(Ints.toByteArray(i)));
         }
+
+        logger.info("[{}]", mhf.getLong(Ints.toByteArray(2056)));
+        logger.info("[{}]", mhf.getLong(Ints.toByteArray(2057)));
     }
 }
