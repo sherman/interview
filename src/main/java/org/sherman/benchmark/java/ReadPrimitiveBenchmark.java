fadc079cf2765d4e58c7c1d4d4ff6b5e1f620444
Denis M. Gabaidulin <gabaden@gmail.com>
2023-11-02 23:35:08 +0300

    * update read primitive test

--- src/main/java/org/sherman/benchmark/java/ReadPrimitiveBenchmark.java
+++ src/main/java/org/sherman/benchmark/java/ReadPrimitiveBenchmark.java
@@ -1,27 +1,19 @@
 package org.sherman.benchmark.java;
 
-import static com.google.common.primitives.Ints.fromBytes;
-
 import com.google.common.primitives.Ints;
+import one.nio.util.JavaInternals;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import sun.misc.Unsafe;
+
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
-import one.nio.util.JavaInternals;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.infra.Blackhole;
-import sun.misc.Unsafe;
+
+import static com.google.common.primitives.Ints.fromBytes;
 
 @Fork(2)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
@@ -35,8 +27,8 @@ public class ReadPrimitiveBenchmark {
     private final byte[] data = new byte[SIZE];
     private final Unsafe unsafe = JavaInternals.getUnsafe();
     private final ByteBuffer byteBuffer = ByteBuffer.wrap(data);
-    private int ints = 0;
-    private long intsAsLong = 0L;
+    private int intIndex = 0;
+    private long indexAsLongIndex = 0L;
 
     @Setup
     public void generate() {
@@ -45,32 +37,37 @@ public class ReadPrimitiveBenchmark {
             for (var b : value) {
                 data[i] = b;
             }
-            ints++;
-            intsAsLong++;
         }
+
+        var index = ThreadLocalRandom.current().nextInt((SIZE / 4) - 32);
+        intIndex = index;
+        indexAsLongIndex = index;
     }
 
     @Benchmark
     public void readArray(Blackhole blackhole) {
-        var index = ThreadLocalRandom.current().nextInt(ints);
-        blackhole.consume(fromBytes(data[index], data[index + 1], data[index + 2], data[index + 3]));
+        var index = intIndex;
+        blackhole.consume(
+            fromBytes(
+                data[index],
+                data[index + 1],
+                data[index + 2],
+                data[index + 3]
+            ));
     }
 
     @Benchmark
     public void readByteBuffer(Blackhole blackhole) {
-        var index = ThreadLocalRandom.current().nextInt(ints);
-        blackhole.consume(byteBuffer.getInt(index));
+        blackhole.consume(byteBuffer.getInt(intIndex));
     }
 
     @Benchmark
     public void readMethodHandle(Blackhole blackhole) {
-        var index = ThreadLocalRandom.current().nextInt(ints);
-        blackhole.consume((int) varHandle.get(data, index));
+        blackhole.consume((int) varHandle.get(data, intIndex));
     }
 
     @Benchmark
     public void readUnsafe(Blackhole blackhole) {
-        long index = ThreadLocalRandom.current().nextLong(intsAsLong);
-        blackhole.consume(unsafe.getInt(data, index));
+        blackhole.consume(unsafe.getInt(data, indexAsLongIndex));
     }
 }
