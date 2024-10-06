613bf9591926cd03df0700148df90a84f9da2e59
Denis M. Gabaidulin <gabaden@gmail.com>
2024-10-06 21:57:10 +0300

    * pay respect to duplicates

--- src/main/java/com/leetcode/assorted_2024/FindCommonCharacters.java
+++ src/main/java/com/leetcode/assorted_2024/FindCommonCharacters.java
@@ -1,10 +1,14 @@
 package com.leetcode.assorted_2024;
 
 import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.BitSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -14,28 +18,38 @@ public class FindCommonCharacters {
     private static final Logger logger = LoggerFactory.getLogger(FindCommonCharacters.class);
 
     public List<String> commonChars(String[] words) {
-        var alphabets = new HashMap<Character, BitSet>();
+        var alphabets = new HashMap<Character, int[]>();
         for (var i = 0; i < 26; i++) {
-            alphabets.put((char) ('a' + i), new BitSet(words.length));
+            alphabets.put((char) ('a' + i), new int[words.length]);
         }
         for (var i = 0; i < words.length; i++) {
             for (var c : words[i].toCharArray()) {
-                alphabets.get(c).set(i);
+                alphabets.get(c)[i]++;
             }
         }
 
         return alphabets.entrySet().stream()
-            .filter(e -> e.getValue().cardinality() == words.length)
-            .map(Map.Entry::getKey)
+            .filter(e -> Arrays.stream(e.getValue()).allMatch(i -> i > 0))
+            .flatMap((Function<Map.Entry<Character, int[]>, Stream<?>>) freq -> {
+                var min = Integer.MAX_VALUE;
+                for (var i = 0; i < freq.getValue().length; i++) {
+                    min = Math.min(min, freq.getValue()[i]);
+                }
+                var letters = new ArrayList<Character>();
+                for (var i = 0; i < min; i++) {
+                    letters.add(freq.getKey());
+                }
+                return letters.stream();
+            })
             .map(String::valueOf)
             .toList();
     }
 
     @Test
     public void test() {
-        Assert.assertEquals(ImmutableList.of("e", "l"), commonChars(new String[] {"bella", "label", "roller"}));
+        Assert.assertEquals(ImmutableList.of("e", "l", "l"), commonChars(new String[] {"bella", "label", "roller"}));
         Assert.assertEquals(ImmutableList.of(), commonChars(new String[] {"a", "b", "c"}));
-        Assert.assertEquals(ImmutableList.of("a"), commonChars(new String[] {"aa", "baa", "caa", "bxaaa"}));
+        Assert.assertEquals(ImmutableList.of("a", "a"), commonChars(new String[] {"aa", "baa", "caa", "bxaaa"}));
 
     }
 }
