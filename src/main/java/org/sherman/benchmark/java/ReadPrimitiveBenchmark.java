4972f6a02f232a7f7944a63bed5e6a43e79cffee
Denis M. Gabaidulin <gabaden@gmail.com>
2024-11-03 22:59:41 +0300

    * improve read primitive benchmark

--- src/main/java/org/sherman/benchmark/java/ReadPrimitiveBenchmark.java
+++ src/main/java/org/sherman/benchmark/java/ReadPrimitiveBenchmark.java
@@ -1,27 +1,36 @@
 package org.sherman.benchmark.java;
 
+import static com.google.common.primitives.Ints.fromBytes;
+
 import com.google.common.base.Preconditions;
 import com.google.common.primitives.Ints;
-import one.nio.util.JavaInternals;
-import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.infra.Blackhole;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import sun.misc.Unsafe;
-
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
-
-import static com.google.common.primitives.Ints.fromBytes;
+import one.nio.util.JavaInternals;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.misc.Unsafe;
 
 @Fork(2)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@BenchmarkMode(Mode.AverageTime)
+@BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @State(Scope.Benchmark)
 public class ReadPrimitiveBenchmark {
@@ -70,41 +79,76 @@ public class ReadPrimitiveBenchmark {
         }
 
         var index = ThreadLocalRandom.current().nextInt(indexes.length);
+        if (index + 31 >= indexes.length) {
+            index = indexes.length - 31;
+        }
         intIndex = indexes[index];
         indexAsLongIndex = indexes[index];
         baseLineIndex = index;
     }
 
+    @OperationsPerInvocation(31)
     @Benchmark
     public void readBaseline(Blackhole blackhole) {
-        blackhole.consume(baseline[baseLineIndex]);
+        var sum = 0;
+        var start = baseLineIndex;
+        var end = start + 31;
+        for (var i = start; i < end; i++) {
+            sum += baseline[i];
+        }
+        blackhole.consume(sum);
     }
 
+    @OperationsPerInvocation(31)
     @Benchmark
     public void readArray(Blackhole blackhole) {
-        var index = intIndex;
-        blackhole.consume(
-            fromBytes(
-                data[index],
-                data[index + 1],
-                data[index + 2],
-                data[index + 3]
-            )
-        );
+        var sum = 0;
+        var start = intIndex;
+        var end = start + 31;
+        for (var i = start; i < end; i++) {
+            sum += fromBytes(
+                data[i],
+                data[i + 1],
+                data[i + 2],
+                data[i + 3]
+            );
+        }
+        blackhole.consume(sum);
     }
 
+    @OperationsPerInvocation(31)
     @Benchmark
     public void readByteBuffer(Blackhole blackhole) {
-        blackhole.consume(byteBuffer.getInt(intIndex));
+        var sum = 0;
+        var start = intIndex;
+        var end = start + 31;
+        for (var i = start; i < end; i++) {
+            sum += byteBuffer.getInt(i);
+        }
+        blackhole.consume(sum);
     }
 
+    @OperationsPerInvocation(31)
     @Benchmark
     public void readMethodHandle(Blackhole blackhole) {
-        blackhole.consume((int) varHandle.get(data, intIndex));
+        var sum = 0;
+        var start = intIndex;
+        var end = start + 31;
+        for (var i = start; i < end; i++) {
+            sum += (int) varHandle.get(data, i);
+        }
+        blackhole.consume(sum);
     }
 
+    @OperationsPerInvocation(31)
     @Benchmark
     public void readUnsafe(Blackhole blackhole) {
-        blackhole.consume(unsafe.getInt(data, indexAsLongIndex));
+        var sum = 0L;
+        var start = indexAsLongIndex;
+        var end = start + 31L;
+        for (var i = start; i < end; i++) {
+            sum += unsafe.getInt(data, i);
+        }
+        blackhole.consume(sum);
     }
 }
