923bdf4b7aae4db5482e57225c7ace44884cd1f5
Denis M. Gabaidulin <gabaden@gmail.com>
2025-01-28 22:39:50 +0300

    1155. Number of Dice Rolls With Target Sum (memoize)

--- src/main/java/com/leetcode/amazon/NumberOfDiceRollsWithTargetSum.java
+++ src/main/java/com/leetcode/amazon/NumberOfDiceRollsWithTargetSum.java
@@ -1,5 +1,7 @@
 package com.leetcode.amazon;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -8,18 +10,19 @@ import org.testng.annotations.Test;
 public class NumberOfDiceRollsWithTargetSum {
     private static final Logger logger = LoggerFactory.getLogger(NumberOfDiceRollsWithTargetSum.class);
 
+    private final Map<Key, Integer> cache = new HashMap<>();
+
     public int numRollsToTarget(int n, int k, int target) {
+        cache.clear();
         var total = 0;
         var sum = 0;
         for (var i = 1; i <= k; i++) {
-            sum += i;
-            total += recursion(n, k, target, sum);
-            sum -= i;
+            total += recursion(n, k, i, target, sum + i);
         }
         return total;
     }
 
-    private static int recursion(int n, int k, int target, int sum) {
+    private int recursion(int n, int k, int item, int target, int sum) {
         //logger.info("recursion n={} k={}", n, k);
         if (n == 1) {
             if (sum == target) {
@@ -28,20 +31,32 @@ public class NumberOfDiceRollsWithTargetSum {
             return 0;
         }
 
+        var cacheKey = new Key(item, sum);
+        var result = cache.get(cacheKey);
+        if (result != null) {
+            return result;
+        }
+
         var total = 0;
         for (var i = 1; i <= k; i++) {
-            sum += i;
-            total += recursion(n - 1, k, target, sum);
-            sum -= i;
+            total += recursion(n - 1, k, i, target, sum + i);
         }
+
+        cache.put(cacheKey, total);
+
         return total;
+    }
+
+    private record Key(int dice, int sum) {
     }
 
     @Test
     public void test() {
+        Assert.assertEquals(numRollsToTarget(10, 20, 60), 3712);
+        Assert.assertEquals(numRollsToTarget(4, 100, 100), 9506);
+        Assert.assertEquals(numRollsToTarget(2, 6, 7), 6);
         Assert.assertEquals(numRollsToTarget(2, 6, 3), 2);
         Assert.assertEquals(numRollsToTarget(1, 6, 3), 1);
-        Assert.assertEquals(numRollsToTarget(2, 6, 7), 6);
         Assert.assertEquals(numRollsToTarget(3, 10, 20), 63);
     }
 }
