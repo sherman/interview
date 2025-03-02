97061db75a2af9c2f197181e3c58cd3b3fb75630
Denis M. Gabaidulin <gabaden@gmail.com>
2025-03-02 21:53:02 +0300

    * 345. Reverse Vowels of a String

---
+++ src/main/java/com/leetcode/assorted_2025/ReverseVowelsOfAString.java
@@ -0,0 +1,38 @@
+package com.leetcode.assorted_2025;
+
+public class ReverseVowelsOfAString {
+    private static final String VOWELS = "aeiouAEIOU";
+
+    public String reverseVowels(String s) {
+        var chars = s.toCharArray();
+        var l = 0;
+        var r = chars.length - 1;
+        while (l < r) {
+            var lc = chars[l];
+            var rc = chars[r];
+            var leftIsVowel = isVowel(lc);
+            var rightIsVowel = isVowel(rc);
+
+            if (leftIsVowel && rightIsVowel) {
+                chars[r] = lc;
+                chars[l] = rc;
+                l++;
+                r--;
+            } else {
+                if (!leftIsVowel) {
+                    l++;
+                } else if (!rightIsVowel) {
+                    r--;
+                } else {
+                    l++;
+                    r--;
+                }
+            }
+        }
+        return new String(chars);
+    }
+
+    private static boolean isVowel(char c) {
+        return VOWELS.indexOf(c) != -1;
+    }
+}
