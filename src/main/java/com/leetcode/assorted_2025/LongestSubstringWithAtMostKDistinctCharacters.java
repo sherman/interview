2a3b33d8332ed33b259244db0bb080591a1faba0
Denis M. Gabaidulin <gabaden@gmail.com>
2025-11-09 12:29:39 +0300

    340. Longest Substring with At Most K Distinct Characters (sliding window)

--- src/main/java/com/leetcode/assorted_2025/LongestSubstringWithAtMostKDistinctCharacters.java
+++ src/main/java/com/leetcode/assorted_2025/LongestSubstringWithAtMostKDistinctCharacters.java
@@ -7,19 +7,30 @@ import org.junit.jupiter.api.Test;
 public class LongestSubstringWithAtMostKDistinctCharacters {
 
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        var maxLen = 0;
+        var maxLen = 0; // best solution
+        var start = 0; // start of the window
+        var chars = new HashMap<Character, Integer>(); // chars stat tracking
 
         for (var i = 0; i < s.length(); i++) {
-            var chars = new HashMap<Character, Integer>();
-            for (var j = i; j < s.length(); j++) {
-                var c = s.charAt(j);
-                var num = chars.getOrDefault(c, 0);
-                chars.put(c, num + 1);
-                if (chars.size() <= k) {
-                    var len = j - i + 1;
-                    maxLen = Math.max(maxLen, len);
+            var c = s.charAt(i);
+            var num = chars.getOrDefault(c, 0);
+            chars.put(c, num + 1);
+            if (chars.size() <= k) {
+                maxLen = Math.max(maxLen, i - start + 1);
+            } else {
+                var charToRemove = s.charAt(start);
+                var num2 = chars.get(charToRemove);
+                if (num2 > 1) {
+                    chars.put(charToRemove, num2 - 1);
+                } else {
+                    chars.remove(charToRemove);
                 }
+                start++;
             }
+        }
+
+        if (chars.size() <= k) {
+            maxLen = Math.max(maxLen, s.length() - start);
         }
 
         return maxLen;
@@ -29,6 +40,7 @@ public class LongestSubstringWithAtMostKDistinctCharacters {
     public void test() {
         Assertions.assertEquals(3, lengthOfLongestSubstringKDistinct("ecebe", 2));
         Assertions.assertEquals(2, lengthOfLongestSubstringKDistinct("aa", 1));
+        Assertions.assertEquals(2, lengthOfLongestSubstringKDistinct("abee", 1));
         Assertions.assertEquals(6, lengthOfLongestSubstringKDistinct("abggdgee", 3));
     }
 }
